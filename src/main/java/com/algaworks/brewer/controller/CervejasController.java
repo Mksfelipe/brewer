13ccfab5670baa2fbc3b2d3adb387fbfafb8fc11
Felipe <sousafelipe123@gmail.com>
2021-01-03 20:12:23 -0300

    Configurando Thymeleaf

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -1,5 +1,16 @@
 package com.algaworks.brewer.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cervejas")
 public class CervejasController {
 
+	@GetMapping("/novo")
+	public String novo() {
+		return "cerveja/CadastroCerveja";
+	}
+	
 }
